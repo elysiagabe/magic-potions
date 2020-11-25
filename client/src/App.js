918c6a0c84ef1bbc3875948111b4488be166a320
Elysia Gabe <elysiagabe@gmail.com>
2020-11-24 21:34:24 -0800

    Removed server directory and placed contents in root

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Box } from '@chakra-ui/react';
 // Components
 import OrderForm from './pages/OrderForm';
